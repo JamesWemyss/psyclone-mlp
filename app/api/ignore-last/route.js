5c449bd462b40ff6ceb2945972f492347bd0e0c9
JamesWemyss <jj@untitledtm.com>
2025-08-14 16:32:47 +0100

    Update route.js

--- app/api/ignore-last/route.js
+++ app/api/ignore-last/route.js
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
